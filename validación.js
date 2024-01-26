16add08af5ce18830a03a7cae177046866908605
daniela-ayalasnc <156262882+daniela-ayalasnc@users.noreply.github.com>
2024-01-25 21:18:25 -0600

    Update validación.js

--- validación.js
+++ validación.js
@@ -2,36 +2,24 @@
       document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("submitButton").addEventListener("click", function(event) {
           event.preventDefault();
-
-          // Obtener los valores del correo y la contraseña
           var email = document.getElementById("emailInput").value;
           var password = document.getElementById("passwordInput").value;
-
-          // Validar el correo electrónico
           if (!validarEmail(email)) {
-            console.log("Error: Correo electrónico no válido");
+            console.log("Correo electrónico no válido");
             return;
           }
-
-          // Validar la contraseña
           if (!validarPassword(password)) {
             console.log("Error: La contraseña debe tener al menos 8 caracteres");
             return;
           }
-
-          // Si ambos son válidos, puedes enviar el formulario o realizar otras acciones
-          console.log("Formulario enviado con éxito");
+          console.log("inicio de sesion exitoso");
         });
       });
 
-      // Función para validar el correo electrónico
       function validarEmail(email) {
-        // Expresión regular simple para validar el formato del correo electrónico
         var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return regex.test(email);
       }
-
-      // Función para validar la contraseña
       function validarPassword(password) {
         return password.length >= 8;
       }
