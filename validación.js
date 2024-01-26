fa2dd7eb48528d54d792f6db37a4a606a23a66b8
daniela-ayalasnc <156262882+daniela-ayalasnc@users.noreply.github.com>
2024-01-25 20:58:15 -0600

    Update validación.js

--- validación.js
+++ validación.js
@@ -1,8 +1,8 @@
-const nombre= document.getElementById("nombre")
-const correo= document.getElementById("correo")
-const contraseña= document.getElementById("contraseña")
-const formulario = document.getElementById("formulario")
-const parrafo = document.getElementById("peligro")
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("correo");
+const contraseña = document.getElementById("contraseña");
+const formulario = document.getElementById("formulario");
+const mensaje = document.getElementById("peligro");
 
 formulario.addEventListener('submit', e => {
     e.preventDefault();
@@ -10,17 +10,17 @@ formulario.addEventListener('submit', e => {
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     let debeEnviar = false;
 
-    if (nombreUsuario.value.length < 5) {
+    if (nombre.value.length < 5) {
         advertencias += 'El nombre de usuario es demasiado corto. ';
         debeEnviar = true;
     }
 
-    if (!regexEmail.test(correoElectronico.value)) {
+    if (!regexEmail.test(correo.value)) {
         advertencias += 'El correo electrónico ingresado no es válido. ';
         debeEnviar = true;
     }
 
-    if (contrasena.value.length < 8) {
+    if (contraseña.value.length < 8) {
         advertencias += 'La contraseña es demasiado corta. ';
         debeEnviar = true;
     }
@@ -29,6 +29,5 @@ formulario.addEventListener('submit', e => {
         mensaje.innerHTML = advertencias;
     } else {
         mensaje.innerHTML = "Formulario enviado con éxito";
-        // Aquí puedes agregar código adicional para enviar los datos del formulario si es necesario.
     }
 });
